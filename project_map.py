fc119561263188537ad2af5e021daf8e9c309a2c
rttariverdi67 <rttariverdi67@gmail.com>
2023-09-05 17:14:49 +0300

    filter map

--- project_map.py
+++ project_map.py
@@ -6,6 +6,7 @@ import mrob
 import numpy as np
 import open3d as o3d
 from pathlib import Path
+from scipy.spatial import cKDTree
 
 parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
@@ -67,25 +68,61 @@ def run_FGraph(x):
 
     return T[0]
 
+def pcd_3D_to_2d(map_pcd):
+    pcd = copy.deepcopy(map_pcd)
+    pcd_points = np.asarray(pcd.points)
+    twod_points = np.zeros_like(pcd_points)
+    twod_points[:, :2] = pcd_points[:, :2]
+    pcd_2d = o3d.geometry.PointCloud()
+    pcd_2d.points = o3d.utility.Vector3dVector(twod_points)
+    return pcd_2d
+
+def filter_point_cloud(pcd, threshold, K, color):
+    
+    pcd_2d = pcd_3D_to_2d(pcd)
+    point_cloud = np.asarray(pcd_2d.points)
+    
+    # Build a kd-tree from the point cloud data
+    kdtree = cKDTree(point_cloud)
+
+    # List to store points with more than K neighbors
+    idxs = []
+
+    # Iterate through all points
+    for idx, point in enumerate(point_cloud):
+        # Query the kd-tree for neighbors within distance d
+        neighbors = kdtree.query_ball_point(point, threshold)
+
+        # Count the number of neighbors
+        num_neighbors = len(neighbors)
+
+        # Check if the point has more than K neighbors
+        if num_neighbors > K:
+            idxs.append(idx)
+            
+    pcd_filtered = o3d.geometry.PointCloud()
+    pcd_filtered.points = o3d.utility.Vector3dVector(np.asarray(pcd.points)[idxs])
+    pcd_filtered.paint_uniform_color(color)
+
+    return pcd_filtered
+
 
 def rectify_map(map_pcd):
-    cl, ind = map_pcd.remove_statistical_outlier(nb_neighbors=5, std_ratio=0.001)
-    map_pcd = map_pcd.select_by_index(ind)
-    
-    map_pcd.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=1.5, max_nn=30))
-    normals = np.asarray(map_pcd.normals)
-    colors = 0.5 * (normals + 1)  # Normalize and shift the range to [0, 1]
-    map_pcd.colors = o3d.utility.Vector3dVector(colors)
-    idxs = (colors[:, 2] < 0.3)
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(np.asarray(map_pcd.points)[idxs])
-
-    # Rotate map to 2D projection
+    # cl, ind = map_pcd.remove_statistical_outlier(nb_neighbors=5, std_ratio=0.001)
+    # map_pcd = map_pcd.select_by_index(ind)
+        
     pca = PCA(n_components=3)
-    pca.fit(np.asarray(pcd.points))
+    pca.fit(np.asarray(map_pcd.points))
     dT = run_FGraph(pca.components_[2])
-    map_rect = copy.deepcopy(pcd).transform(dT)
+    map_rect = copy.deepcopy(map_pcd).transform(dT)
     
+    map_points = np.asarray(map_rect.points)
+    _2D_points = np.zeros_like(map_points)
+    _2D_points[:, :2] = map_points[:, :2]
+    
+    pcd_2d = o3d.geometry.PointCloud()
+    pcd_2d.points = o3d.utility.Vector3dVector(_2D_points)
+        
     return dT, map_rect
     
     
@@ -98,14 +135,15 @@ def project_map(args):
     #rectify map
     print('rectify map ...')
     dT, map_pcd = rectify_map(map_pcd)
-    o3d.io.write_point_cloud(os.path.join(Path(args.save_path), "map_rect.pcd"), map_pcd)
     
-    #paint/initialize points colors
-    map_pcd.paint_uniform_color([0,0,0])
+    map_pcd_filtered = filter_point_cloud(map_pcd, threshold= 0.3, K= 40, color=[0,0,0])
+    
+    o3d.io.write_point_cloud(os.path.join(Path(args.save_path), "map_rect.pcd"), map_pcd_filtered)
+    
 
     # Project points to 2D map image
-    min_coord = np.min(np.asarray(map_pcd.points)[:, :2], axis=0)
-    max_coord = np.max(np.asarray(map_pcd.points)[:, :2], axis=0)
+    min_coord = np.min(np.asarray(map_pcd_filtered.points)[:, :2], axis=0)
+    max_coord = np.max(np.asarray(map_pcd_filtered.points)[:, :2], axis=0)
 
     meters_resoltion = max_coord - min_coord
     meter2pix = 100
@@ -114,8 +152,8 @@ def project_map(args):
     image_width = (np.ceil(meters_resoltion[1] * meter2pix) // k + 1) * k
     image = np.ones((int(image_height), int(image_width), 3))
 
-    points = np.asarray(map_pcd.points)
-    colors = np.asarray(map_pcd.colors)
+    points = np.asarray(map_pcd_filtered.points)
+    colors = np.asarray(map_pcd_filtered.colors)
 
     for i in range(points.shape[0]):
         coords = ((points[i][:2] - min_coord) * meter2pix).astype(int)
@@ -134,7 +172,7 @@ def project_map(args):
     np.save(Path(args.save_path) / 'dT.npy', dT)
 
     print('saving map.png ...')
-    # plt.imsave(Path(args.save_path) / "map.png", image)
+    plt.imsave(Path(args.save_path) / "map.png", image)
 
 if __name__ == '__main__':
     project_map(args)
