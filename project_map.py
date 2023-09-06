8a3b6cea7407a1e2c23fb13dc3f54e62d30e784b
rttariverdi67 <rttariverdi67@gmail.com>
2023-09-06 18:45:49 +0300

    update

--- project_map.py
+++ project_map.py
@@ -1,6 +1,6 @@
 from sklearn.decomposition import PCA
 import argparse
-import os, copy
+import os, copy, math
 import matplotlib.pyplot as plt
 import mrob
 import numpy as np
@@ -36,8 +36,9 @@ def T_rect_check(solution):
 
 def run_FGraph(x):
     
-    if not T_rect_check(x):
-        x = -1 * x
+    # if not T_rect_check(x):
+    #     print('T_rect checked.')
+    #     x = -1 * x
     
     # How to align a vector with gravity, ensuring that only the rotation is changed
     # For this we need two factors, one looking at the rotation and the second looking at the pose to remain at the origin.
@@ -67,6 +68,53 @@ def run_FGraph(x):
     T = graph.get_estimated_state()
 
     return T[0]
+
+def pose2D(pose):
+    rect_origin = pose.T() @ np.array([0, 0, 0, 1])
+    rect_z_axis = pose.T() @ np.array([0, 1, 0, 1])
+    
+    x_coord = rect_origin[0]
+    y_coord = rect_origin[1]
+    
+    pose_dx = rect_z_axis[0] - rect_origin[0]
+    pose_dy = rect_z_axis[1] - rect_origin[1]
+
+    return x_coord, y_coord, pose_dx, pose_dy
+
+def ax(pose, size = 10):
+    axis = o3d.geometry.TriangleMesh.create_coordinate_frame(size=size)
+    return axis.transform(pose)
+
+def plot_map_pose(X, Y, init_pose, dT):
+    
+    dT = mrob.geometry.SE3(dT)
+    
+    pose = dT * init_pose
+    x, y, dx, dy = pose2D(pose)
+    
+    fig, ax = plt.subplots(figsize=(12,12))
+
+    ax.scatter(X, Y, marker='.', s=1)
+
+    ax.arrow(
+        x, y, dx, dy,
+        width=0.1,
+        alpha=1,
+        color="red",
+        head_width=3,
+        head_length=math.sqrt((dx*5) ** 2 + (dy*5) ** 2),
+        length_includes_head=True,
+        head_starts_at_zero=False,
+        )
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_title('some title :)')
+
+    plt.axis('equal')
+    plt.show()
+    
+    
 
 def pcd_3D_to_2d(map_pcd):
     pcd = copy.deepcopy(map_pcd)
@@ -102,14 +150,14 @@ def filter_point_cloud(pcd, threshold, K, color):
             
     pcd_filtered = o3d.geometry.PointCloud()
     pcd_filtered.points = o3d.utility.Vector3dVector(np.asarray(pcd.points)[idxs])
-    pcd_filtered.paint_uniform_color(color)
+    pcd_filtered.paint_uniform_color(color) 
+    
+    pcd_2d.points = o3d.utility.Vector3dVector(np.asarray(pcd_2d.points)[idxs])
 
-    return pcd_filtered
+    return pcd_filtered, pcd_2d
 
 
 def rectify_map(map_pcd):
-    # cl, ind = map_pcd.remove_statistical_outlier(nb_neighbors=5, std_ratio=0.001)
-    # map_pcd = map_pcd.select_by_index(ind)
         
     pca = PCA(n_components=3)
     pca.fit(np.asarray(map_pcd.points))
@@ -136,31 +184,15 @@ def project_map(args):
     print('rectify map ...')
     dT, map_pcd = rectify_map(map_pcd)
     
-    map_pcd_filtered = filter_point_cloud(map_pcd, threshold= 0.3, K= 40, color=[0,0,0])
+    map_pcd_filtered, map_pcd_2d = filter_point_cloud(map_pcd, threshold= 0.3, K= 40, color=[0,0,0])
+    map_pcd_2d = map_pcd_2d.voxel_down_sample(0.5)
+    
+    cl, ind = map_pcd_2d.remove_statistical_outlier(nb_neighbors=40, std_ratio=0.5)
+    map_pcd_2d = map_pcd_2d.select_by_index(ind)
     
     o3d.io.write_point_cloud(os.path.join(Path(args.save_path), "map_rect.pcd"), map_pcd_filtered)
+    o3d.io.write_point_cloud(os.path.join(Path(args.save_path), "map_rect_2d.pcd"), map_pcd_2d)
     
-
-    # Project points to 2D map image
-    min_coord = np.min(np.asarray(map_pcd_filtered.points)[:, :2], axis=0)
-    max_coord = np.max(np.asarray(map_pcd_filtered.points)[:, :2], axis=0)
-
-    meters_resoltion = max_coord - min_coord
-    meter2pix = 100
-    k = 256
-    image_height = (np.ceil(meters_resoltion[0] * meter2pix) // k + 1) * k
-    image_width = (np.ceil(meters_resoltion[1] * meter2pix) // k + 1) * k
-    image = np.ones((int(image_height), int(image_width), 3))
-
-    points = np.asarray(map_pcd_filtered.points)
-    colors = np.asarray(map_pcd_filtered.colors)
-
-    for i in range(points.shape[0]):
-        coords = ((points[i][:2] - min_coord) * meter2pix).astype(int)
-        image[coords[0], coords[1]] = colors[i]
-        
-
-    dT[:2, 3] = dT[:2, 3] - np.array([min_coord[0], min_coord[1]])
     T_coord = np.array([
         [0, 1, 0, 0],
         [1, 0, 0, 0],
@@ -171,8 +203,8 @@ def project_map(args):
     print('saving rectification matrix ...')
     np.save(Path(args.save_path) / 'dT.npy', dT)
 
-    print('saving map.png ...')
-    plt.imsave(Path(args.save_path) / "map.png", image)
+    # print('saving map.png ...')
+    # plt.imsave(Path(args.save_path) / "map.png", image)
 
 if __name__ == '__main__':
     project_map(args)
